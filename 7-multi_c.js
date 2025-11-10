f703b5a452f912ee305368c91fee71141e288814
Richard Adamah <richardadamant@gmail.com>
2025-11-10 00:51:55 +0000

    added the 7th file

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+const numOccurrences = parseInt(firstArg);
+
+if (isNaN(numOccurrences)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log("C is fun");
+  }
+}
